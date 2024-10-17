a4437aafbf0479ae0895b90c9e96872c7b227c36
Ryann1908 <59339990+RyannThi@users.noreply.github.com>
2024-10-17 14:41:37 -0300

    Iniciando UI com Pause

---
+++ Online_HnS/Assets/Scripts/UI/PauseManager.cs
@@ -0,0 +1,128 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PauseManager : MonoBehaviour
+{
+    #region Object References
+    [Header("--- Object References")]
+    [SerializeField]
+    private RawImage[] scrollers; // The scrolling checkers in the top and bottom
+
+    [SerializeField]
+    private Image backdrop; // The black background
+
+    [SerializeField]
+    private List<GameObject> buttons = new List<GameObject>(); // Interactible buttons
+    private List<RectTransform> buttonsRects = new List<RectTransform>(); // RectTransforms of said buttons
+    #endregion
+
+    #region Timelines
+    [Header("--- Timelines")]
+    [SerializeField]
+    private int currentTimeline; // ------------------------- !!! Reminder to update this based on what the current timeline is
+
+    [SerializeField]
+    private Color[] mainColors; // The main color palette to be used in the images
+                                // 0 = past, 1 = present, 2 = future
+    #endregion
+
+    #region Animation Settings
+    [Header("--- Animation Settings")]
+    [SerializeField]
+    private float radius = 100f; // Offset of the buttons from the main handle
+
+    [SerializeField]
+    private float angleOffset = 45f; // Difference of angle between buttons
+    #endregion
+
+    #region Animations (Coroutines)
+    private Coroutine[] scrollersCoroutines = new Coroutine[2];
+    private Coroutine backdropCoroutine;
+
+    #endregion
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        ResetPauseMenu();
+        PopInAnimate();
+        Debug.Log(buttons.Count.ToString());
+        for (int i = 0; i < buttons.Count; i++)
+        {
+            Debug.Log(i.ToString());
+            buttonsRects.Add(buttons[i].GetComponent<RectTransform>());
+        }
+
+        int buttonCount = buttons.Count;
+        int middleIndex = buttonCount / 2; // Índice do botão central
+
+        for (int i = 0; i < buttonCount; i++)
+        {
+            float currentAngle = (middleIndex - i) * angleOffset; // Distribui os botões simetricamente
+            float radians = currentAngle * Mathf.Deg2Rad;
+
+            // Calcula a posição usando seno e cosseno
+            Vector2 position = new Vector2(Mathf.Cos(radians), Mathf.Sin(radians)) * radius;
+
+            // Atualiza a posição do botão
+            buttonsRects[i].anchoredPosition = position;
+            buttonsRects[i].localEulerAngles = new Vector3(0, 0, currentAngle);
+        }
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        ScrollScrollers();
+    }
+
+    /// <summary>
+    /// Resets the pause menu back to its default state and refreshes the colors
+    /// </summary>
+    private void ResetPauseMenu()
+    {
+        currentTimeline = 1;
+
+        backdrop.color = new Color(0, 0, 0, 0);
+
+        scrollers[0].rectTransform.anchoredPosition = new Vector3(0, -25.5f, 0);
+        scrollers[1].rectTransform.anchoredPosition = new Vector3(0, 25.5f, 0);
+        scrollers[0].color = mainColors[currentTimeline];
+        scrollers[1].color = mainColors[currentTimeline];
+        scrollers[0].uvRect = new Rect(0, 0, 1, 1);
+        scrollers[1].uvRect = new Rect(0, 0, 1, 1);
+    }
+
+    /// <summary>
+    /// Scrolls the scrollers on the top and bottom of the screen.
+    /// </summary>
+    private void ScrollScrollers()
+    {
+        scrollers[0].uvRect = new Rect(scrollers[0].uvRect.x - (0.001f * Time.deltaTime), 0, 1, 1);
+        scrollers[1].uvRect = new Rect(scrollers[1].uvRect.x - (0.001f * Time.deltaTime), 0, 1, 1);
+    }
+
+    /// <summary>
+    /// Invoke the animation for all of the elements of the pause menu, including transformation and coloring + alpha.<br></br>
+    /// The StopAllAnimations() method will be called before the animation starts.
+    /// </summary>
+    private void PopInAnimate()
+    {
+        StopAllAnimations();
+        scrollersCoroutines[0] = StartCoroutine(UIUtils.MoveOverSecondsRaw(scrollers[0], new Vector3(0, 25.5f, 0), 0.35f));
+        scrollersCoroutines[1] = StartCoroutine(UIUtils.MoveOverSecondsRaw(scrollers[1], new Vector3(0, -25.5f, 0), 0.35f));
+        backdropCoroutine = StartCoroutine(UIUtils.FadeColor(backdrop, new Color(0, 0, 0, 125), 0.5f));
+    }
+
+    /// <summary>
+    /// Attempts to stop all of the coroutines related to the pause menu animations.
+    /// </summary>
+    private void StopAllAnimations()
+    {
+        try { StopCoroutine(scrollersCoroutines[0]); } catch { }
+        try { StopCoroutine(scrollersCoroutines[1]); } catch { }
+        try { StopCoroutine(backdropCoroutine); } catch { }
+    }
+}
---
+++ Online_HnS/Assets/Scripts/Utils/UIUtils.cs
@@ -0,0 +1,47 @@
+using System.Collections;
+using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIUtils : MonoBehaviour
+{
+    /// <summary>
+    /// Moves a given raw image from one position to another over a certain amount of time
+    /// </summary>
+    /// <param name="imageToMove"></param>
+    /// <param name="end"></param>
+    /// <param name="seconds"></param>
+    /// <returns></returns>
+    public static IEnumerator MoveOverSecondsRaw(RawImage imageToMove, Vector3 end, float seconds)
+    {
+        float elapsedTime = 0;
+        Vector3 startingPos = imageToMove.rectTransform.anchoredPosition;
+        while (elapsedTime < seconds)
+        {
+            imageToMove.rectTransform.anchoredPosition = Vector3.Lerp(startingPos, end, (elapsedTime / seconds));
+            elapsedTime += Time.deltaTime;
+            yield return new WaitForEndOfFrame();
+        }
+        imageToMove.rectTransform.anchoredPosition = end;
+    }
+
+    /// <summary>
+    /// Fades a given image from one color to another over a certain amount of time
+    /// </summary>
+    /// <param name="imageToFade"></param>
+    /// <param name="end"></param>
+    /// <param name="duration"></param>
+    /// <returns></returns>
+    public static IEnumerator FadeColor(Image imageToFade, Color end, float duration)
+    {
+        Color startColor = imageToFade.color;
+        for (float t = 0f; t < duration; t += Time.deltaTime)
+        {
+            float normalizedTime = t / duration;
+            imageToFade.color = Color.Lerp(startColor, end, normalizedTime);
+            yield return null;
+        }
+        imageToFade.color = end;
+    }
+}
